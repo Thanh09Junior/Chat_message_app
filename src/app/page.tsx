d10349677fd7d7811203af909bb5f0ff23a37a34
Thanh09Junior <juniorthanh09@gmail.com>
2024-11-06 08:23:55 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           className={styles.logo}
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
+          width={170}
           height={38}
           priority
         />
